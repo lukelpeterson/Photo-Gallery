b4378b50c18115ed6d0188bacefc2fe85c8c7208
lukelpeterson <lukelpeterson@gmail.com>
2016-02-19 15:46:38 -0800

    Version 1

---
+++ sitejs/app.js
@@ -0,0 +1,106 @@
+// Add light box to Gallery
+
+var $overlay = $('<div id="overlay" class="animsition"></div>');
+var $image = $("<img class='animsition'>");
+var $caption = $("<p class='animsition'></p>");
+var $previous = $('<button id="previous"><</button>');
+var $next = $('<button id="next">></button>');
+
+$('img').addClass('animsition');
+
+// Add image to overlay
+$overlay.append($image);
+
+// Add overlay
+$("body").append($overlay);
+
+// Add Caption
+$overlay.append($caption);
+
+// Add Next and Previous
+$image.before($previous);
+$image.after($next);
+
+// Click event link to image
+$(".photo-gallery a").click(function(event){
+  event.preventDefault();
+  // Display Image
+  getCurrentImage(this);
+
+  // Show the overlay
+  $overlay.show();
+
+    // Set Caption
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+});
+
+
+// Get Next Image
+$("#next").click(function(event){
+  getNextImage();
+});
+
+// Get Previous Image
+$("#previous").click(function(event){
+  getPrevImage();
+});
+
+function getCurrentImage (currentImage) {
+    thisImage = currentImage;
+    var imageLocation = $(currentImage).attr("href");
+    //Update overlay with the image linked in the link
+    $image.attr("src", imageLocation);
+
+    //Get child's alt attribute and set caption
+    var captionText = $(currentImage).children("img").attr("alt");
+    $caption.text(captionText);
+}
+
+function getNextImage() {
+    imageParent = $(thisImage).parent().next();
+    if(imageParent.length!==0){
+    thisImage = $(imageParent).children("a");
+    }
+    getCurrentImage(thisImage);
+}
+
+function getPrevImage() {
+    imageParent = $(thisImage).parent().prev();
+    if(imageParent.length!==0){
+      thisImage = $(imageParent).children("a");
+    }
+    getCurrentImage(thisImage);
+
+}
+// Close overlay when clicked
+$overlay.click(function(){
+    //Hide overlay
+    if(event.target.id == "overlay")
+    $overlay.hide();
+});
+
+// Animation
+
+$(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
